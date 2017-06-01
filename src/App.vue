65741b8db310569d55ede17c622b4501df5c9616
Evan Hickman <travisevanhickman@gmail.com>
2017-05-31 19:03:05 -0500

    boilerplate

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <passport></passport>
+  </div>
+</template>
+
+<script>
+import Passport from './components/Passport'
+
+export default {
+  name: 'app',
+  components: {
+    Passport
+  }
+}
+</script>
+
+<style>
+  @import "./styles/app.scss";
+</style>
---
+++ src/components/Passport.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div class="passport">
+    <div class="grid">
+      <div class="grid_item">
+        <div class="column">
+          <div class="column_item avatar"></div>
+          <div class="column_item id-number"></div>
+        </div>
+      </div>
+      <div class="grid_item">
+        <div class="column">
+          <div class="column_item column-item_top"></div>
+          <div class="column_item column-item_bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'passport'
+//    data () {
+//      return {
+//      }
+//    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  @import "../styles/app.scss";
+</style>
